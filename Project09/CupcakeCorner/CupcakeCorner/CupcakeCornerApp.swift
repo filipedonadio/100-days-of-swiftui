4d84298789a127ca9c325125c275b13c21192039
Filipe Donadio <filipe@filipedonadio.com>
2022-03-04 14:55:04 +0100

    Complete Cupcake Corner app project

---
+++ Project09/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Filipe Donadio on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
